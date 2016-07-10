d963aa31d3aaad96c4edb920b92ae369c29008dc
Hemslo Wang <hemslo.wang@gmail.com>
2016-07-11 01:40:05 +0800

    Add new todo functionality

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,16 @@
 class Todo < ApplicationRecord
+  validates :title, presence: true
+
+  before_create :set_default_completed
+  before_save :strip_title
+
+  private
+
+  def strip_title
+    title.strip!
+  end
+
+  def set_default_completed
+    self.completed = false if completed.nil?
+  end
 end
